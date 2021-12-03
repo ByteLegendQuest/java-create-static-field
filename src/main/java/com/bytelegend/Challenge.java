2c477a56a94a706e2ce5eca48915de667eb7282e
CaptainOfPhB <captainofphb@gmail.com>
2021-12-03 11:01:07 +0800

    Update Challenge.java

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,6 +1,8 @@
 package com.bytelegend;
 
 public class Challenge {
+    publi static String message = "hello";
+    
     public static void main(String[] args) {
         System.out.println(message + ", world!");
     }

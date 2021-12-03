5ad07f98ccd02a23a8c061e6f7026a0c2c168819
CaptainOfPhB <captainofphb@gmail.com>
2021-12-03 11:02:20 +0800

    fix typo

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,7 +1,7 @@
 package com.bytelegend;
 
 public class Challenge {
-    publi static String message = "hello";
+    public static String message = "hello";
     
     public static void main(String[] args) {
         System.out.println(message + ", world!");

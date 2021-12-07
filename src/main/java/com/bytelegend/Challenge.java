f14824cec98a4e60d021c34292e7191eb41af805
MeteorAndy <AndyLamwxl@Outlook.com>
2021-12-07 02:12:11 +0000

    Update at 2021-12-07T02:12:11.703831Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -4,4 +4,7 @@ public class Challenge {
     public static void main(String[] args) {
         System.out.println(message + ", world!");
     }
+    public static String message(){
+        return "Hello"
+    }
 }

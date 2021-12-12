15cf3c282071d72920cc6c8c0c11d3c8b2262687
barthon-b <checkmy@github.profile>
2021-12-12 22:04:08 +0100

    Adding static field message to class Challenge as per ByteLegend training instructions

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,6 +1,10 @@
 package com.bytelegend;
 
 public class Challenge {
+	
+	private static String message = "Hello";
+	
+	
     public static void main(String[] args) {
         System.out.println(message + ", world!");
     }

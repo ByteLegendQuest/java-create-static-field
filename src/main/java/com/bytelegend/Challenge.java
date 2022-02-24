da3f6511af2c6f4c8ea9fe9e33957584880683fb
FriedreichBismarck <2360756905@qq.com>
2022-02-24 09:17:21 +0000

    Update at 2022-02-24T09:17:21.416136Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,10 +1,8 @@
 package com.bytelegend;
 
 public class Challenge {
-    public static String message() {
-        return "Hello";
-    }
+    public static String message="Hello";
     public static void main(String[] args) {
-        System.out.println(message() + ", world!");
+        System.out.println(message + ", world!");
     }
 }

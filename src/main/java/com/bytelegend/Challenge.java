ba3c00f3a45de9c82d762ee70c5ffe750775f9a1
wl212 <2568247954@qq.com>
2022-03-19 14:14:32 +0000

    Update at 2022-03-19T14:14:32.033556Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,6 +2,6 @@ package com.bytelegend;
 
 public class Challenge {
     public static void main(String[] args) {
-        System.out.println(message + ", world!");
+        System.out.println(message + "Hello, world!");
     }
 }

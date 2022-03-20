9dbe681932c0f307c856c7a3d7991aacb34da617
wangjie-fourth <wangjie_fourth@163.com>
2022-03-20 11:21:06 +0000

    Update at 2022-03-20T11:21:06.086468Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,9 +2,9 @@ package com.bytelegend;
 
 public class Challenge {
 
-    public static final String MESSAGE = "Hello";
+    public static String message = "Hello";
 
     public static void main(String[] args) {
-        System.out.println(MESSAGE + ", world!");
+        System.out.println(message + ", world!");
     }
 }

4e2fa581f566dbe5c85de1a8710875c5f2719405
Zuoxq99 <957974737@qq.com>
2022-03-23 07:41:40 +0000

    Update at 2022-03-23T07:41:40.717492Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,7 +1,11 @@
 package com.bytelegend;
 
 public class Challenge {
+
+    public String message;
+
     public static void main(String[] args) {
+        message = "Hello";
         System.out.println(message + ", world!");
     }
 }

c94ed0caab001453da68d432523548052f2cb0ce
Zasch <zdevel@freenet.de>
2022-03-25 07:53:43 +0000

    Update at 2022-03-25T07:53:43.725593Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,8 +1,9 @@
 package com.bytelegend;
 
-public static final String message = "Hello";
-
 public class Challenge {
+
+    public static final String message = "Hello";
+
     public static void main(String[] args) {
         System.out.println(message + ", world!");
     }

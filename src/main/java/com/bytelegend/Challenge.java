aaeef4aafabbb0d518af23aaf440fb01597390e2
MegaMidnight <richarr916@gmail.com>
2022-03-29 15:37:12 +0000

    Update at 2022-03-29T15:37:12.336428Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,6 +1,9 @@
 package com.bytelegend;
 
 public class Challenge {
+    
+    public static final String message = "Hello";
+
     public static void main(String[] args) {
         System.out.println(message + ", world!");
     }

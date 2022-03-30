77560157ea5957dc4aeec8dc427ac52340792e68
hupizhu <2815695640@qq.com>
2022-03-30 12:08:53 +0000

    Update at 2022-03-30T12:08:53.027608Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,7 +2,7 @@ package com.bytelegend;
 
 public class Challenge {
     public static void main(String[] args) {
-        public static Stirng = "Hello";
+        public static String = "Hello";
         System.out.println(message + ", world!");
     }
 }

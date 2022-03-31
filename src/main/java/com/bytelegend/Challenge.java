514c9e5f9944878d4be582dc53a7fe4efc242ab7
SN-Beaux <Beaux.Luo@supernovacompanies.biz>
2022-03-31 03:41:45 +0000

    Update at 2022-03-31T03:41:45.284448Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,6 +2,8 @@ package com.bytelegend;
 
 public class Challenge {
     public static void main(String[] args) {
+        String message = "Hello";
+        
         System.out.println(message + ", world!");
     }
 }

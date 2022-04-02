32ea416e7a74680d674cb2074d970cc0731abc61
ghost0913 <1208843528@qq.com>
2022-04-02 06:31:14 +0000

    Update at 2022-04-02T06:31:14.171512Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,6 +2,7 @@ package com.bytelegend;
 
 public class Challenge {
     public static void main(String[] args) {
+        public static String message = "Hello";
         System.out.println(message + ", world!");
     }
 }

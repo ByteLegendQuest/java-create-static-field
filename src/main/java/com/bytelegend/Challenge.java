86a66e00ea38718fc747af5f137c68bc9a71edb1
Tomcat-Guo <zhiqiang.guo@shopee.com>
2022-04-09 14:22:20 +0000

    Update at 2022-04-09T14:22:20.364464Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -3,6 +3,6 @@ package com.bytelegend;
 public class Challenge {
     static String message = "Hello, ";
     public static void main(String[] args) {
-        System.out.println(message + ", world!");
+        System.out.println(message + "world!");
     }
 }

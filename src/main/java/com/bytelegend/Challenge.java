af22b3f83d6bbb3a71b989dc210b2f704cab74f2
g497813927 <admin@techzjc.com>
2022-04-15 06:05:19 +0000

    Update at 2022-04-15T06:05:19.030036Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,11 +1,8 @@
 package com.bytelegend;
 
 public class Challenge {
+    private static String message = "Hello";
     public static void main(String[] args) {
         System.out.println(message + ", world!");
-    }
-
-    public static String message() {
-        return "Hello";
     }
 }

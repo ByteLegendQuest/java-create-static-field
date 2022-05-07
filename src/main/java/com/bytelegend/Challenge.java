4221a349e7d9785bd0d749818925ccbaee92177e
kangming06 <kangming06@hotmail.com>
2022-05-07 04:51:46 +0000

    Update at 2022-05-07T04:51:46.049967Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,6 +1,7 @@
 package com.bytelegend;
 
 public class Challenge {
+    public static String="Hello"
     public static void main(String[] args) {
         System.out.println(message + ", world!");
     }

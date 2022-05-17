d4e9660d7f4509d92f67c460bd0c88b436b3b773
andrewbaxter439 <andy@andybaxter.me>
2022-05-17 22:51:54 +0000

    Update at 2022-05-17T22:51:54.392154Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,6 +1,9 @@
 package com.bytelegend;
 
 public class Challenge {
+
+    private static String message = "Hello";
+
     public static void main(String[] args) {
         System.out.println(message + ", world!");
     }

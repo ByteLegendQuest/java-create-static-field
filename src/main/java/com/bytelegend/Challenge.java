fd0272a33f50c6479663947b1bd33946d406c4d8
jamesmkinder <jmkgfk@gmail.com>
2022-05-18 03:32:01 +0000

    Update at 2022-05-18T03:32:01.472731Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,5 +1,7 @@
 package com.bytelegend;
 
+static String message = "Hello";
+
 public class Challenge {
     public static void main(String[] args) {
         System.out.println(message + ", world!");

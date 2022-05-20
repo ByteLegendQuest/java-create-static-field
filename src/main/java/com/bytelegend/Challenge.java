022290b4a9491ec5c28c39b7ef8f104e5c9ad4d5
周啟尧 <435184766@qq.com>
2022-05-20 18:28:34 +0800

    static field

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,6 +1,8 @@
 package com.bytelegend;
 
 public class Challenge {
+    static String message = "Hello";
+
     public static void main(String[] args) {
         System.out.println(message + ", world!");
     }

ba0074a52d07e8fa928079aba058b867a5e4210e
chinahzh <2987140244@qq.com>
2022-05-27 03:02:33 +0000

    Update at 2022-05-27T03:02:33.409550Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -4,5 +4,5 @@ public class Challenge {
     public static void main(String[] args) {
         System.out.println(message + ", world!");
     }
-    public static String message = "Hello"
+    public static String message = "Hello";
 }

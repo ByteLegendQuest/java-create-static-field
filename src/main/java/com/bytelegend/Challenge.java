aa5b6dd12fc4ed048351c9c62ba02a9b41ef623d
1250285855 <1250285855@qq.com>
2022-06-25 15:54:54 +0000

    Update at 2022-06-25T15:54:54.843628Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,9 +1,10 @@
 package com.bytelegend;
 
 public class Challenge {
-    public static void main(String[] args) {
 
-        String message = "Hello";
+    public static String message = "Hello";
+
+    public static void main(String[] args) {
 
         System.out.println(message + ", world!");
     }

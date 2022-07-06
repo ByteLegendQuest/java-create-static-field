d461ddc61b9917718792724d1f93cbf9c926f33f
chushi0 <wangyongchao49@sina.com>
2022-07-06 15:07:46 +0000

    Update at 2022-07-06T15:07:46.594605Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,6 +1,9 @@
 package com.bytelegend;
 
 public class Challenge {
+
+public static String message = "Hello";
+
     public static void main(String[] args) {
         System.out.println(message + ", world!");
     }

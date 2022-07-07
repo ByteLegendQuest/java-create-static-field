83f4881eb1696243124791f3f9c4ad89178e6a38
agirnob <kakcayigitbaran@gmail.com>
2022-07-07 14:29:33 +0000

    Update at 2022-07-07T14:29:33.874583Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,7 +2,7 @@ package com.bytelegend;
 
 public class Challenge {
     public static void main(String[] args) {
-        System.out.println(message() + ", world!");
+        System.out.println(message + ", world!");
     }
 
     public static String message="Hello"

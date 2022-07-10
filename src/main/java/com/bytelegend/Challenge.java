ed3e014adfa9734368479b134bbaf365a2c8620a
Jungmin-Moon <jmmoon96@gmail.com>
2022-07-10 20:25:13 +0000

    Update at 2022-07-10T20:25:13.420411Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,8 +2,8 @@ package com.bytelegend;
 
 public class Challenge {
 
-    public static String = "Hello";
-    
+    public static String message = "Hello";
+
     public static void main(String[] args) {
         System.out.println(message + ", world!");
     }

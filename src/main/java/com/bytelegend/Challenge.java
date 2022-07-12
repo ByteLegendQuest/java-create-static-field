38244b48a2afc8cc627f786b1cf875bd4bf336f7
chesterbachelor <kawalerbartosz@gmail.com>
2022-07-12 14:50:19 +0000

    Update at 2022-07-12T14:50:19.930061Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,5 +1,7 @@
 package com.bytelegend;
 
+public static String message = "Hello"
+
 public class Challenge {
     public static void main(String[] args) {
         System.out.println(message + ", world!");

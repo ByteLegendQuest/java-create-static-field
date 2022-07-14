825e9f67897f529b738c6a02fef7f70d9c5293c1
tassellll <2112562745@qq.com>
2022-07-14 03:57:00 +0000

    Update at 2022-07-14T03:57:00.510709Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,12 +1,8 @@
 package com.bytelegend;
 
 public class Challenge {
+    public static String message = "Hello";
     public static void main(String[] args) {
-        System.out.println(message() + ", world!");
+        System.out.println(message + ", world!");
     }
-    public static String message() {
-        String hello = "Hello";
-        return hello;
-    }
-    
 }

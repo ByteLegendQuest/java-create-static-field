55756dfe310737003ba9f7c292fa39a45ab6f4ee
Crystalyyds <2031291160@qq.com>
2022-07-22 04:37:12 +0000

    Update at 2022-07-22T04:37:12.071132Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,7 +2,7 @@ package com.bytelegend;
 
 public class Challenge {
     public static void main(String[] args) {
-        String message = "Hello";
         System.out.println(message + ", world!");
     }
+    public static String message = "Hello";
 }

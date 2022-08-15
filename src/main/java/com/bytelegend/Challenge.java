4daf15dac6bcc37e370e94a7c89aa8578dce726e
NotAWeeaboo <billyyoo052@gmail.com>
2022-08-15 06:34:19 +0000

    Update at 2022-08-15T06:34:19.584107Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,7 +1,12 @@
 package com.bytelegend;
 
 public class Challenge {
+
+    public static String message = "Hello";
+
     public static void main(String[] args) {
+        
         System.out.println(message + ", world!");
+
     }
 }

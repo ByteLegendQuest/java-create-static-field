295b425e22e19a48a377515aab1eba93726b3fd8
MisterToma <ktoma@sandi.net>
2022-08-20 04:52:14 +0000

    Update at 2022-08-20T04:52:14.836306Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,10 +2,9 @@ package com.bytelegend;
 
 public class Challenge {
     public static void main(String[] args) {
+        public static String message = "Hello";
         System.out.println(message + ", world!");
     }
 
-    public static String message(){
-        return "Hello";
-    }
+    
 }

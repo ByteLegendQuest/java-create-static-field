7d9872973e26bc00186bfbf0c353eb81eb2e1045
GergertHatby <yanghao9225@outlook.com>
2022-08-31 08:29:00 +0000

    Update at 2022-08-31T08:29:00.351975Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,7 +2,7 @@ package com.bytelegend;
 
 public class Challenge {
     public static void main(String[] args) {
-        public static String message = "Hello"
+        public static String message = "Hello";
         System.out.println(message + ", world!");
     }
 }

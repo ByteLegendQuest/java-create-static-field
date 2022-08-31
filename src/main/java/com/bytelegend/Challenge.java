52480dcae3e87c2723e2505e0ee9db52d632fbae
ov0o0vo <3035566793@qq.com>
2022-08-31 13:55:48 +0000

    Update at 2022-08-31T13:55:48.411649Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,8 +1,11 @@
 package com.bytelegend;
 
 public class Challenge {
+    
+    public static String message = "hello";
+
     public static void main(String[] args) {
         System.out.println(message + ", world!");
     }
-    public static String message = "hello";
+    
 }

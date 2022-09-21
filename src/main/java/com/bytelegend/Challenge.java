be943f1daaa8fc5741a601cd3e30ad4662e3a187
PuPp1T <nanhu9604@gmail.com>
2022-09-21 01:44:04 +0000

    Update at 2022-09-21T01:44:04.253267Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,6 +1,7 @@
 package com.bytelegend;
 
 public class Challenge {
+    public static message = "Hello";
     public static void main(String[] args) {
         System.out.println(message + ", world!");
     }

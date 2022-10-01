a0158e92dad4ea70180dcac0736537fcfabb44d4
MHPYGH <2950552644@qq.com>
2022-10-01 10:50:11 +0000

    Update at 2022-10-01T10:50:11.377467Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,7 +1,11 @@
 package com.bytelegend;
 
 public class Challenge {
+
+    public static String massage = "Hello";
+
     public static void main(String[] args) {
+
         System.out.println(message + ", world!");
     }
 }

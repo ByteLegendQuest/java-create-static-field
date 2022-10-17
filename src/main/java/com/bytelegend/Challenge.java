cb67f9f1ec73ef0c189618695b44d6d2d4d86718
hhamm1314 <hhamm0624@126.com>
2022-10-17 09:27:20 +0000

    Update at 2022-10-17T09:27:19.999718Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,7 +1,22 @@
 package com.bytelegend;
 
+/**
+ * 这是一个方法
+ * @Author haohao
+ * @Date 2022/10/17
+ */
 public class Challenge {
+
+     /**
+     * message
+     */
+    public static final String MESSAGE = "Hello";
+
+    /**
+     * 主方法
+     * @param args
+     */
     public static void main(String[] args) {
-        System.out.println(message + ", world!");
+        System.out.println(MESSAGE + ", world!");
     }
 }

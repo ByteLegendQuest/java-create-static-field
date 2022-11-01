b37402c575403d72202b6b6aefc7f6900dea97bb
bdjdndn <909439250@qq.com>
2022-11-01 03:03:33 +0000

    Update at 2022-11-01T03:03:33.402392Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,8 +1,7 @@
 package com.bytelegend;
 
 public class Challenge {
-        public static  String message="hello";
-
+    public static  String message="hello";
     public static void main(String[] args) {
         System.out.println(message + ", world!");
     }

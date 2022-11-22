e89fa768a4f90463822b90032e49e78ccb03bc97
williamgl <ligan@oregonstate.edu>
2022-11-22 22:34:50 +0000

    Update at 2022-11-22T22:34:50.686345Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,6 +1,8 @@
 package com.bytelegend;
 
 public class Challenge {
+    public static message = "Hello";
+
     public static void main(String[] args) {
         System.out.println(message + ", world!");
     }

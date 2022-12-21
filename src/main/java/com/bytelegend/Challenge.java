235864c5e7c6077b1e683fcc00a02c1f722eeeb8
19138060480 <703791321@qq.com>
2022-12-21 15:06:06 +0000

    Update at 2022-12-21T15:06:06.303537Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,6 +2,7 @@ package com.bytelegend;
 
 public class Challenge {
     public static void main(String[] args) {
+        static String message="Hello";
         System.out.println(message + ", world!");
     }
 }

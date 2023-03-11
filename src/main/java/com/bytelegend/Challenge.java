e0dca760b4aab286ceb0706c44e70d9a95824b8b
judemaij <judemaij@gmail.com>
2023-03-11 21:39:59 +0000

    Update at 2023-03-11T21:39:59.757029Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,6 +2,9 @@ package com.bytelegend;
 
 public class Challenge {
     public static void main(String[] args) {
-        System.out.println(message + ", world!");
+        System.out.println(message + "world!");
+    }
+    public static String message(String message) {
+        return message = "Hello";
     }
 }

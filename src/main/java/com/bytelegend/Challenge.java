00a2d4b2f74b3b60334a42aaea75e0ddcc74d2f1
xcrou89 <moise.89@outlook.com>
2023-05-21 18:41:30 +0000

    Update at 2023-05-21T18:41:29.999837Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,7 +2,7 @@ package com.bytelegend;
 
 public class Challenge {
 
-public static String message = "Hello, world!";
+    public static String message = "Hello";
 
     public static void main(String[] args) {
         System.out.println(message + ", world!");

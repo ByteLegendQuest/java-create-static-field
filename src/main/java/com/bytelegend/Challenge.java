7ff9b180a9cb332e4ae9690b957938626052d018
rosettastoned12 <officiallynabiel@gmail.com>
2023-05-23 07:42:12 +0000

    Update at 2023-05-23T07:42:12.066621Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,6 +1,9 @@
 package com.bytelegend;
 
 public class Challenge {
+    public static String message() {
+        return "Hello";
+    }
     public static void main(String[] args) {
         System.out.println(message + ", world!");
     }

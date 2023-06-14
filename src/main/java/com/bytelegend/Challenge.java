84e56549832e7ba674a70ce197add7cd4dcbc43d
cjh200300 <1053123738@qq.com>
2023-06-14 10:24:15 +0800

    2023-06-14 10:23:54

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,7 +1,7 @@
 package com.bytelegend;
 
 public class Challenge {
-    static String message;
+    static String message = "Hello";
     public static void main(String[] args) {
         System.out.println(message + ", world!");
     }
